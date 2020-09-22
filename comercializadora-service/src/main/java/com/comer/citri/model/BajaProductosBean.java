60fa7eeabf86f84fd6e78bae1e925cd0579e1f11
Sergio151410 <sergio-jair@hotmail.com>
2020-09-22 13:27:33 -0500

    se agregan componente

---
+++ comercializadora-service/src/main/java/com/comer/citri/model/BajaProductosBean.java
@@ -0,0 +1,30 @@
+/**
+ * Banco Santander (México) S.A., Institución de Banca Múltiple, Grupo Financiero Santander México
+ * Todos los derechos reservados
+ * BajaProductosBean.java
+ * Control de versiones:
+ * Version  Date/Hour               By                  				Company     Description
+ * -------  -------------------     --------------------------------    --------    -----------------------------------------------------------------
+ * 1.0      21 sep. 2020 18:53:45	    [Sergio Jair Flores Luna]     			  SYM		    Creacion de BajaProductosBean.java
+ */
+package com.comer.citri.model;
+
+import java.io.Serializable;
+
+/**
+ * Descripcion:
+ *
+ * @author sergi
+ *
+ */
+public class BajaProductosBean implements Serializable{
+
+	/**
+	 * 
+	 * Nombre: serialVersionUID
+	 * Tipo: long
+	 * Descripcion: Declaracion de variable serialVersionUID del tipo long
+	 */
+	private static final long serialVersionUID = -7854090590695372864L;
+
+}
---
+++ comercializadora-service/src/main/java/com/comer/citri/model/ModificacionProductosBean.java
@@ -0,0 +1,30 @@
+/**
+ * Banco Santander (México) S.A., Institución de Banca Múltiple, Grupo Financiero Santander México
+ * Todos los derechos reservados
+ * ModificacionProductosBean.java
+ * Control de versiones:
+ * Version  Date/Hour               By                  				Company     Description
+ * -------  -------------------     --------------------------------    --------    -----------------------------------------------------------------
+ * 1.0      21 sep. 2020 18:55:43	    [Sergio Jair Flores Luna]     			  SYM		    Creacion de ModificacionProductosBean.java
+ */
+package com.comer.citri.model;
+
+import java.io.Serializable;
+
+/**
+ * Descripcion:
+ *
+ * @author sergi
+ *
+ */
+public class ModificacionProductosBean implements Serializable{
+
+	/**
+	 * 
+	 * Nombre: serialVersionUID
+	 * Tipo: long
+	 * Descripcion: Declaracion de variable serialVersionUID del tipo long
+	 */
+	private static final long serialVersionUID = -7531174391851630823L;
+
+}
